8df72fc47de92965ded52cf34a62177fdacfbe4f
https://MAC4NO5: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 <macanos1@eafit.edu.co>
2023-05-14 17:06:17 -0500

    Add new primitive recursive functions to our practice

--- PRF.hs
+++ PRF.hs
@@ -1,12 +1,12 @@
 data Nat = Zero | Succ Nat
 
-instance Show Nat where 
+instance Show Nat where
     show :: Nat -> String
     show Zero = "Zero"
-    show (Succ n) = "Succ(" ++ show n ++ ")" 
+    show (Succ n) = "Succ(" ++ show n ++ ")"
 
 -- Is a type of recursor used in all natural numbers
-recNat :: a -> (Nat -> a -> a) -> Nat -> a 
+recNat :: a -> (Nat -> a -> a) -> Nat -> a
 recNat a _ Zero = a
 recNat a h (Succ n) = h n (recNat a h n)
 
@@ -14,5 +14,19 @@ recNat a h (Succ n) = h n (recNat a h n)
 addR :: Nat -> Nat -> Nat
 addR m n = recNat m (\_ y -> Succ y) n
 
-predecesor :: Nat -> Nat
-predecesor m = recNat Zero (\_ y -> m) m
+-- Primitive recursive function to multiply two numbers
+multiR :: Nat -> Nat -> Nat
+multiR m n = recNat Zero (\_ y -> addR m y) n
+
+--Primitive recursive function to get the identity of a number
+idP :: Nat -> Nat
+idP n = recNat n (\_ y -> y) Zero
+
+-- Primitive recursive function that give the predeccesor of a number
+predecessor :: Nat -> Nat
+predecessor Zero = Zero
+predecessor (Succ m) = recNat m (\_ y -> y) Zero
+
+--Primitive recursive function to multiply same number a X certain number times
+pow :: Nat -> Nat -> Nat
+pow m n= recNat (Succ Zero) (\_ y -> multiR m y) n
