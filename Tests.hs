d6b25e51239a7809e2410338c9028fb83744ca57
https://MAC4NO5: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 <macanos1@eafit.edu.co>
2023-05-15 19:53:11 -0500

    Added comments in properties of functions

--- Tests.hs
+++ Tests.hs
@@ -3,7 +3,7 @@ import Test.QuickCheck
 import Test.QuickCheck.Instances.Natural ()
 import GHC.Natural (Natural)
 
---Properties of add function to use tests of QuickCheck
+--Properties of add function, return a number added with other
 prop_addR :: Natural -> Natural -> Bool
 prop_addR m n = natToNatural(addR m' n') == m + n
     where
@@ -11,7 +11,7 @@ prop_addR m n = natToNatural(addR m' n') == m + n
     m' = naturalToNat m
     n' = naturalToNat n
 
---Properties of add function to use tests of QuickCheck
+--Properties of multiply number, return a number multiply with other
 prop_multiR :: Natural -> Natural -> Bool
 prop_multiR m n = natToNatural(multiR m' n') == m * n
     where
@@ -19,13 +19,14 @@ prop_multiR m n = natToNatural(multiR m' n') == m * n
     m' = naturalToNat m
     n' = naturalToNat n
 
---Properties of add function to use tests of QuickCheck
+--Properties of identity number, return the same number
 prop_idR :: Natural -> Bool
 prop_idR n = natToNatural(idR n') == n
     where
     n' :: Nat
     n' = naturalToNat n
 
+--Properties of predeccessor of a number, return a previous number
 prop_predeccesor :: Natural -> Bool
 prop_predeccesor n
     | n == 0 = True
@@ -34,12 +35,14 @@ prop_predeccesor n
     n' :: Nat
     n' = naturalToNat n
 
+--Properties of doubleSucc of a number, return a number with double succ
 prop_doubleSuccR :: Natural -> Bool
 prop_doubleSuccR n = natToNatural(doubleSuccR n') == n + 2
     where
     n' :: Nat
     n' = naturalToNat n
-    --ghc -o lab2 -package quickcheck-instances Tests.hs
+
+--Here our tests run
 main :: IO ()
 main = do
     quickCheck prop_addR
